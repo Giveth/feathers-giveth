f2668f8f7e53e1d13d43c765ce0eab6cd8da8070
ojones <wakeupoj@gmail.com>
2017-08-01 21:43:22 -0700

    givers service and ignorn yarnlock

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const skunkworks = require('./skunkworks/skunkworks.service.js');
 const causes = require('./causes/causes.service.js');
 const projects = require('./projects/projects.service.js');
 const milestones = require('./milestones/milestones.service.js');
+const givers = require('./givers/givers.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(skunkworks);
   app.configure(causes);
   app.configure(projects);
   app.configure(milestones);
+  app.configure(givers);
 };
