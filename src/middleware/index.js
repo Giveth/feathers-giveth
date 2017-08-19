39469076c78bab93c38b05d00e4a93e19502d9bb
perissology <perissology@protonmail.com>
2017-08-18 17:44:38 -0700

    revert a few more files from merge

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,5 @@
-import authenticate from './authenticate';
-
 module.exports = function () {
   // Add your custom middleware here. Remember, that
   // in Express the order matters
   const app = this;
-
-  app.use(authenticate);
 };
