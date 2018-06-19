96ce4c225600005a65970c70551d45f40e7a30f5
Vojtech Simetka <vojtech@simetka.cz>
2018-06-19 02:56:20 +0200

    Fixes #95

--- src/services/ethconversion/ethconversion.filters.js
+++ src/services/ethconversion/ethconversion.filters.js
@@ -1,4 +1,5 @@
-module.exports = function(data, connection, hook) {
-  // eslint-disable-line no-unused-vars
-  return data;
-};
+console.warn(
+  'You are using the default filter for the ethconversion service. For more information about event filters see https://docs.feathersjs.com/api/events.html#event-filtering',
+);
+
+module.exports = data => data;
--- src/services/ethconversion/getEthConversionService.js
+++ src/services/ethconversion/getEthConversionService.js
@@ -1,16 +1,82 @@
 const rp = require('request-promise');
 const logger = require('winston');
 
-const fiat = ['BRL', 'CAD', 'CHF', 'CZK', 'EUR', 'GBP', 'MXN', 'THB', 'USD'];
+const fiat = ['AUD', 'BRL', 'CAD', 'CHF', 'CZK', 'EUR', 'GBP', 'MXN', 'THB', 'USD'];
 const MINUTE = 1000 * 60;
 
-const _buildResponse = (timestamp, rates) => {
-  rates.ETH = 1; // adding eth-eth conversion in the response
+const _buildResponse = (timestamp, rates) => ({ timestamp, rates });
 
-  return {
-    timestamp,
-    rates,
-  };
+/**
+ * Get responses from the DB
+ *
+ * @throws Error if there has been DB issue
+ *
+ * @param {Object} app        Feathers app object
+ * @param {Number} timestamp  Timestamp for which the rates should be retrieved
+ *
+ * @return {Object} Object in format { _id, timestamp, rates: { EUR: 100, USD: 90 } }
+ */
+const _getRatesDb = async (app, timestamp) => {
+  const resp = await app.service('ethconversion').find({ query: { timestamp }, internal: true });
+
+  if (resp.data.length > 0)
+    return { _id: resp.data[0]._id, timestamp: resp.data[0].timestamp, rates: resp.data[0].rates };
+
+  // There is no rate in cache
+  return { timestamp, rates: {} };
+};
+
+/**
+ * Get the rates from the cryptocompare API
+ *
+ * @throws Error if fetching the rates from cryptocompare API failed
+ *
+ * @param {Number} timestamp   Timestamp for which the value should be retrieved
+ * @param {Array}  ratestToGet Rates that are missing in the DB and should be retrieved
+ *
+ * @return {Object} Rates object in format { EUR: 241, USD: 123 }
+ */
+const _getRatesCryptocompare = async (timestamp, ratesToGet) => {
+  logger.debug(`Fetching eth coversion from crypto compare for: ${ratesToGet}`);
+
+  const timestampMS = Math.round(timestamp / 1000);
+
+  const promises = ratesToGet.map(f =>
+    rp(
+      `https://min-api.cryptocompare.com/data/dayAvg?fsym=ETH&tsym=${f}&toTs=${timestampMS}&extraParams=giveth`,
+    ),
+  );
+
+  const responses = await Promise.all(promises);
+
+  const rates = {};
+  responses.forEach(resp => {
+    const response = JSON.parse(resp);
+
+    Object.keys(response).forEach(key => {
+      if (fiat.includes(key)) rates[key] = response[key];
+    });
+  });
+
+  return rates;
+};
+
+/**
+ * Save the rates to the DB by either creating new record or updating existing one
+ *
+ * @param {Object} app        Feathers app object
+ * @param {Number} timestamp  Timestamp for which the rates should be stored
+ * @param {Object} rates      Rates to be stored in the DB
+ * @param {Number} id         Optional ID if there already is a record
+ *
+ * @return {Promise} Resolves if the rates were saved correctly
+ */
+const _saveToDB = (app, timestamp, rates, _id = undefined) => {
+  // There already are some rates for this timestamp, update the record
+  if (_id) return app.service('ethconversion').patch(_id, { rates });
+
+  // Create new record
+  return app.service('ethconversion').create({ timestamp, rates });
 };
 
 /**
@@ -19,6 +85,8 @@ const _buildResponse = (timestamp, rates) => {
  *
  * @param {Object} app             Feathers app object
  * @param {Number} requestedData   Optional requested date as number of miliseconds since 1.1.1970 UTC
+ *
+ * @return {Promise} Promise that resolves to object {timestamp, rates: { EUR: 100, USD: 90 } }
  */
 export const getEthConversion = (app, requestedDate) => {
   // Get yesterday date
@@ -31,67 +99,42 @@ export const getEthConversion = (app, requestedDate) => {
 
   // Make sure that the date given is not in future or today
   // Only the rates for yesterday or older dates are final
-  const startDate = reqDateUTC < yesterdayUTC ? reqDateUTC : yesterdayUTC;
-
-  // Create timestamp in seconds, as accepted by cryptocompare
-  const timestamp = Math.round(startDate) / 1000;
+  const timestamp = reqDateUTC < yesterdayUTC ? reqDateUTC : yesterdayUTC;
 
   logger.debug(`request eth conversion for timestamp ${timestamp}`);
 
-  // check if we already have this exchange rate for this timestamp, if not we save it
-  return new Promise((resolve, reject) => {
-    app
-      .service('ethconversion')
-      .find({ query: { timestamp }, internal: true })
-      .then(rates => {
-        if (rates.data.length > 0) {
-          resolve(_buildResponse(rates.data[0].timestamp, rates.data[0].rates));
-        } else {
-          logger.debug('fetching eth coversion from crypto compare');
+  // Check if we already have this exchange rate for this timestamp, if not we save it
+  return new Promise(async (resolve, reject) => {
+    try {
+      const dbRates = await _getRatesDb(app, timestamp);
 
-          // fetch daily avg for each fiat
-          const promises = fiat.map(f =>
-            rp(
-              `https://min-api.cryptocompare.com/data/dayAvg?fsym=ETH&tsym=${f}&toTs=${timestamp}&extraParams=giveth`,
-            ),
-          );
-          const exchangeRates = {};
+      const retrievedRates = new Set(Object.keys(dbRates.rates));
+      const unknownRates = fiat.filter(cur => !retrievedRates.has(cur));
 
-          Promise.all(promises)
-            .then(responses => {
-              responses.forEach(resp => {
-                resp = JSON.parse(resp);
+      if (unknownRates.length === 0) {
+        resolve(_buildResponse(dbRates.timestamp, dbRates.rates));
+      } else {
+        logger.debug('fetching eth coversion from crypto compare');
+        // Some rates have not been obtained yet, get them from cryptocompare
+        const newRates = await _getRatesCryptocompare(timestamp, unknownRates);
+        const allRates = Object.assign({}, dbRates.rates, newRates);
 
-                Object.keys(resp).forEach(key => {
-                  if (fiat.includes(key)) {
-                    exchangeRates[key] = resp[key];
-                  }
-                });
-              });
+        // Save the newly retrieved rates
+        await _saveToDB(app, dbRates.timestamp, allRates, dbRates._id);
 
-              app
-                .service('ethconversion')
-                .create({
-                  timestamp,
-                  rates: exchangeRates,
-                })
-                .then(() => {
-                  resolve(_buildResponse(timestamp, exchangeRates));
-                });
-            })
-            .catch(e => {
-              logger.error('could not fetch eth conversions from crypto compare', e);
-              reject();
-            });
-        }
-      });
+        resolve(_buildResponse(dbRates.timestamp, allRates));
+      }
+    } catch (e) {
+      reject(e);
+    }
   });
 };
 
-// query gas price every minute
+// Query the conversion rate every minute
 export const queryEthConversion = app => {
   getEthConversion(app);
 
+  // TODO: Do we actually need to do this in interval? Can't we just let it update when users request exchange rate?
   setInterval(() => {
     getEthConversion(app);
   }, MINUTE);
