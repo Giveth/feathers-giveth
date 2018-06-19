3c0e09474375799281f560a4f7b9cd905d38e63a
perissology <perissology@protonmail.com>
2018-06-19 12:37:00 -0700

    update resetDB script to clear blockchain db as well

--- scripts/resetDataBase.js
+++ scripts/resetDataBase.js
@@ -1,28 +1,29 @@
 const mongoose = require('mongoose');
+
 const mongoUrl = 'mongodb://localhost:27017/giveth';
 const Confirm = require('prompt-confirm');
+const rimraf = require('rimraf');
 
-new Confirm('Drop database?')
-  .run()
-  .then((reset) => {
-    if(reset) {
-      mongoose.connect(mongoUrl);
-      const db = mongoose.connection;
-      
-      db.on('error', err => {
-        console.error('Could not connect to Mongo', err)
+new Confirm('Drop database?').run().then(reset => {
+  if (reset) {
+    mongoose.connect(mongoUrl);
+    const db = mongoose.connection;
+
+    db.on('error', err => {
+      console.error('Could not connect to Mongo', err);
+      process.exit();
+    });
+
+    // once mongo connected, start migration
+    db.once('open', () => {
+      console.log('Connected to Mongo');
+
+      // remove blockchain db
+      rimraf.sync('../data');
+      db.dropDatabase().then(res => {
+        console.log('database dropped');
         process.exit();
       });
-
-      // once mongo connected, start migration
-      db.once('open', () => {
-        console.log('Connected to Mongo');
-
-        db.dropDatabase().then((res) => {
-          console.log('database dropped');
-          process.exit();
-        });
-      });
-    }
-  });
-
+    });
+  }
+});
