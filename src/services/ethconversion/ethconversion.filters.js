f65ae0bcc62cdc38a771bfe2747a2a5864807780
perissology <perissology@protonmail.com>
2018-06-19 09:04:47 -0700

    remove filter warnings

--- src/services/ethconversion/ethconversion.filters.js
+++ src/services/ethconversion/ethconversion.filters.js
@@ -1,5 +1,1 @@
-console.warn(
-  'You are using the default filter for the ethconversion service. For more information about event filters see https://docs.feathersjs.com/api/events.html#event-filtering',
-);
-
 module.exports = data => data;
