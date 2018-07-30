2f2f32a69caff8499838e83fa417cc69e324e288
Vojtech Simetka <vojtech@simetka.cz>
2018-07-30 21:47:09 +0200

    Fixing unnormalised uncommitted donations

--- src/services/donations/donations.service.js
+++ src/services/donations/donations.service.js
@@ -3,11 +3,22 @@ const createService = require('feathers-mongoose');
 const logger = require('winston');
 const { DonationStatus, createModel } = require('../../models/donations.model');
 const hooks = require('./donations.hooks');
+const { LiquidPledging } = require('giveth-liquidpledging');
+const { getWeb3 } = require('../../blockchain/lib/web3Helpers');
 
 // If a donation has a intendedProject & the commitTime has passed, we need to update the donation to reflect
 // that the intendedProject is now the owner
-const pollForCommittedDonations = service => {
+const pollForCommittedDonations = (app, service) => {
   const interval = 1000 * 30; // check every 30 seconds
+
+  const web3 = getWeb3(app);
+
+  const { liquidPledgingAddress } = app.get('blockchain');
+
+  if (!liquidPledgingAddress) {
+    throw new Error('liquidPledgingAddress is not defined in the configuration file');
+  }
+  const liquidPledging = new LiquidPledging(web3, liquidPledgingAddress);
 
   const doUpdate = async () => {
     try {
@@ -26,6 +37,8 @@ const pollForCommittedDonations = service => {
 
       donations.forEach(async donation => {
         try {
+          await liquidPledging.normalizePledge(donation.pledgeId);
+
           await service.patch(donation._id, {
             status: DonationStatus.COMMITTED,
             amountRemaining: '0',
@@ -39,13 +52,12 @@ const pollForCommittedDonations = service => {
             'commitTime',
             'previousState',
           ];
-          const newDonation = Object.keys(donation)
-            .filter(k => keysToPick.includes(k))
-            .reduce((accumulator, key) => {
-              // eslint-disable-next-line no-param-reassign
-              accumulator[key] = donation[key];
-              return accumulator;
-            }, {});
+
+          const newDonation = {};
+          keysToPick.forEach(key => {
+            newDonation[key] = donation[key];
+          });
+
           Object.assign(newDonation, {
             ownerId: donation.intendedProjectId,
             ownerType: donation.intendedProjectType,
@@ -83,7 +95,7 @@ module.exports = function serviceFactory() {
   // Get our initialized service so that we can register hooks and filters
   const service = app.service('donations');
 
-  // pollForCommittedDonations(service);
+  pollForCommittedDonations(app, service);
 
   service.hooks(hooks);
 };
