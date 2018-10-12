abbceeef7f3cee87e724aac0aab5deef49a7e2e5
perissology <perissology@protonmail.com>
2018-10-12 09:24:41 -0700

    add scripts/keys.js file

---
+++ scripts/keys.js
@@ -0,0 +1,7 @@
+// update this file w/ you pks used for deployments.
+// NOTE: It is recommended to use a throwaway account as the private keys are not encrypted
+module.exports = {
+  mainnet: '',
+  ropsten: '',
+  rinkeby: '',
+};
