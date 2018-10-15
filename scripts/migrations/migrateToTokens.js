31e9b8b766665e19ce5f011912751aa6eadcbdb9
Satya van Heummen <>
2018-10-15 14:20:51 +0200

    Update migration script and ethconversion model

--- scripts/migrations/migrateToTokens.js
+++ scripts/migrations/migrateToTokens.js
@@ -9,6 +9,7 @@ const db = mongoose.connection;
 const Milestones = db.collection('milestones')
 const Campaigns = db.collection('campaigns')
 const Dacs = db.collection('dacs')
+const ETHConversion = db.collection('ethconversions')
 
 db.on('error', err => console.error('Could not connect to Mongo', err));
 
@@ -87,6 +88,54 @@ const migrateDacsToTokens = () => {
 }
 
 
+const migrateEthConversions = () => {
+  return new Promise((resolve, reject) => {
+
+    // remove and create new indexes
+    ETHConversion
+      .getIndexes()
+      .then(indexes => {
+        if(Object.keys(indexes).includes('timestamp_1')) {
+          ETHConversion.dropIndex('timestamp_1')
+            .then( res => console.log('dropped timestamp index on ethconversions'))
+            .catch( err => console.log('could not drop timestamp index on ethconversions'))
+        } else {
+          console.log('index timestamp already dropped')
+        }
+      })
+      .catch(err => console.log('could not get indexes'))
+
+    ETHConversion
+      .getIndexes()
+      .then(indexes => {
+        if(!Object.keys(indexes).includes('timestamp_1_symbol_1')) {
+          ETHConversion.createIndex({ timestamp: 1, symbol: 1}, { unique: true })
+            .then( res => console.log('created symbol/timestamp index on ethconversions'))
+            .catch( err => console.log('could not create symbol/timestamp index on ethconversions'))            
+        } else {
+          console.log('index timestamp/symbol already created')
+        }
+      })
+      .catch(err => console.log('could not get indexes'))
+
+    ETHConversion
+      .updateMany({}, {
+        $set: {
+          symbol: symbol
+        }
+      })
+      .then(res => {
+        console.log(`migrated ${res.result.nModified} of total ${res.result.n} ethconversions`)
+        resolve()
+      })
+      .catch( err => {
+        console.log("error migrating ethconversions ", err)
+        reject()
+      })
+  })
+}
+
+
 
 // once mongo connected, start migration
 db.once('open', () => {
@@ -96,7 +145,8 @@ db.once('open', () => {
   Promise.all([ 
     migrateMilestonesToTokens(), 
     migrateCampaignsToTokens(), 
-    migrateDacsToTokens() 
+    migrateDacsToTokens(),
+    migrateEthConversions(),
   ])
     .then( res => process.exit())
     .catch( err => process.exit())
