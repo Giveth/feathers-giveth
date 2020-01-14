74637f90fac2e90f23112132a3b66b992653c585
Amin Latifi <a.latifi.al@gmail.com>
2020-01-14 14:49:00 +0330

    Events are gotten from blockchain * Conflicts are found on  donations

--- scripts/utils/findConflicts.js
+++ scripts/utils/findConflicts.js
@@ -8,7 +8,7 @@ require('mongoose-long')(mongoose);
 require('../../src/models/mongoose-bn')(mongoose);
 const { LiquidPledging, LiquidPledgingState } = require('giveth-liquidpledging');
 
-const web3Helper = require('../../src/blockchain/lib/web3Helpers');
+// const web3Helper = require('../../src/blockchain/lib/web3Helpers');
 
 const configFileName = 'default'; // default or beta
 
@@ -20,6 +20,8 @@ const tokenSymbolToForeignAddress = {};
 config.tokenWhitelist.forEach(token => {
   tokenSymbolToForeignAddress[token.symbol] = token.foreignAddress.toLowerCase();
 });
+
+const tokensForeignAddress = config.tokenWhitelist.map(t => t.foreignAddress.toLowerCase());
 
 const { nodeUrl, liquidPledgingAddress } = config.blockchain;
 
@@ -42,6 +44,8 @@ const Milestones = require('../../src/models/milestones.model').createModel(app)
 const Campaigns = require('../../src/models/campaigns.model').createModel(app);
 const Donations = require('../../src/models/donations.model').createModel(app);
 
+const { DonationStatus } = require('../../src/models/donations.model');
+
 // Instantiate Web3 module
 // @params {string} url blockchain node url address
 const instantiateWeb3 = url => {
@@ -59,35 +63,46 @@ const instantiateWeb3 = url => {
 
 // Gets status of liquidpledging storage
 // @param {boolean} updateCache whether get new status from blockchain or load from cached file
-const getStatus = async updateCache => {
+const getBlockchainData = async updateCache => {
   const cacheFile = `./liquidPledgingState_${configFileName}.json`;
-  let status;
+  const eventsFile = `./liquidPledgingEvents_${configFileName}.json`;
+
   if (updateCache) {
     const foreignWeb3 = instantiateWeb3(nodeUrl);
     const liquidPledging = new LiquidPledging(foreignWeb3, liquidPledgingAddress);
     const liquidPledgingState = new LiquidPledgingState(liquidPledging);
 
-    const [numberOfPledges, numberOfPledgeAdmins] = await web3Helper.executeRequestsAsBatch(
-      foreignWeb3,
-      [
-        liquidPledging.$contract.methods.numberOfPledges().call.request,
-        liquidPledging.$contract.methods.numberOfPledgeAdmins().call.request,
-      ],
-    );
-    console.log('Number of pledges', numberOfPledges);
-    console.log('Number of pledge admins', numberOfPledgeAdmins);
+    // const [numberOfPledges, numberOfPledgeAdmins] = await web3Helper.executeRequestsAsBatch(
+    //   foreignWeb3,
+    //   [
+    //     liquidPledging.$contract.methods.numberOfPledges().call.request,
+    //     liquidPledging.$contract.methods.numberOfPledgeAdmins().call.request,
+    //   ],
+    // );
+    // console.log('Number of pledges', numberOfPledges);
+    // console.log('Number of pledge admins', numberOfPledgeAdmins);
 
-    status = await liquidPledgingState.getState();
+    const [status, events] = await Promise.all([
+      liquidPledgingState.getState(),
+      // Just transfer events
+      liquidPledging.$contract.getPastEvents('Transfer', {
+        fromBlock: 0,
+        toBlock: 'latest',
+      }),
+    ]);
 
     fs.writeFileSync(cacheFile, JSON.stringify(status, null, 2));
-  } else {
-    status = JSON.parse(fs.readFileSync(cacheFile));
-  }
+    fs.writeFileSync(eventsFile, JSON.stringify(events, null, 2));
 
-  return status;
+    return { status, events };
+  }
+  return {
+    status: JSON.parse(fs.readFileSync(cacheFile)),
+    events: JSON.parse(fs.readFileSync(eventsFile)),
+  };
 };
 
-const findEntityConflicts = (model, projectBalanceMap, fixConflicts = false) => {
+const findEntityConflicts = (model, projectPledgeMap, fixConflicts = false, pledges) => {
   const cursor = model
     .find({
       projectId: { $exists: true },
@@ -95,27 +110,33 @@ const findEntityConflicts = (model, projectBalanceMap, fixConflicts = false) => 
     .cursor();
 
   return cursor.eachAsync(async entity => {
-    const balance = projectBalanceMap.get(String(entity.projectId)) || {};
+    const balance = projectPledgeMap.get(String(entity.projectId)) || {};
+
+    const balancePledged = balance.Pledged || {};
+
     const { donationCounters } = entity;
 
     let conflictFound = false;
     const setObject = {};
 
-    const promises = donationCounters.map(async (dc, index) => {
+    /*
+     Update entity donationCounters
+    */
+    donationCounters.forEach((dc, index) => {
       const { symbol, currentBalance: dbBalance } = dc;
       const foreignAddress = tokenSymbolToForeignAddress[symbol];
-      const donationCounter = balance[foreignAddress];
+      const tokenBalance = balancePledged[foreignAddress];
 
-      if (donationCounter === undefined) {
+      if (tokenBalance === undefined) {
         console.warn(
           `There is no balance for token ${symbol} in blockchain for ${model.modelName} ${entity._id}`,
         );
         return;
       }
 
-      if (dbBalance.toString() !== donationCounter.amount.toFixed(0)) {
+      if (dbBalance.toString() !== tokenBalance.amount.toFixed(0)) {
         const dbBalanceFromWei = Web3.utils.fromWei(dbBalance.toString());
-        const blockchainBalanceFromWei = Web3.utils.fromWei(donationCounter.amount.toFixed(0));
+        const blockchainBalanceFromWei = Web3.utils.fromWei(tokenBalance.amount.toFixed(0));
 
         console.log(
           'conflict found on',
@@ -128,27 +149,19 @@ const findEntityConflicts = (model, projectBalanceMap, fixConflicts = false) => 
           dbBalanceFromWei,
           'value in smart contract',
           blockchainBalanceFromWei,
-          donationCounter.pledges,
+          tokenBalance.pledges,
         );
 
         if (fixConflicts) {
           conflictFound = true;
 
-          setObject[`donationCounters.${index}.currentBalance`] = donationCounter.amount.toFixed();
-          // for (const pledgeId of donationCounter.pledges) {
-          //   const pledge = pledges[pledgeId];
-          //   const donations = await Donations.find({
-          //     pledgeId,
-          //   });
-          // }
+          setObject[`donationCounters.${index}.currentBalance`] = tokenBalance.amount.toFixed();
         }
       }
     });
 
-    await Promise.all(promises);
-
     if (conflictFound) {
-      return model
+      await model
         .update(
           { _id: entity._id },
           {
@@ -160,45 +173,115 @@ const findEntityConflicts = (model, projectBalanceMap, fixConflicts = false) => 
         .exec();
     }
 
-    return Promise.resolve();
+    /*
+    Update donations
+     */
+    const [paidDonations, payingDonations, committedDonations] = await Promise.all(
+      [DonationStatus.PAID, DonationStatus.PAYING, DonationStatus.COMMITTED].map(status => {
+        return Donations.find({
+          ownerTypeId: entity._id,
+          status,
+        }).exec();
+      }),
+    );
+
+    // Find conflict in donations
+    [
+      {
+        pledgeStatus: 'Pledged',
+        donationStatus: DonationStatus.COMMITTED,
+        donations: committedDonations,
+      },
+      {
+        pledgeStatus: 'Paying',
+        donationStatus: DonationStatus.PAYING,
+        donations: payingDonations,
+      },
+      {
+        pledgeStatus: 'Paid',
+        donationStatus: DonationStatus.PAID,
+        donations: paidDonations,
+      },
+    ].forEach(item => {
+      const { pledgeStatus, donationStatus, donations } = item;
+
+      tokensForeignAddress.forEach(tokenAddress => {
+        if (!balance[pledgeStatus]) return;
+
+        const thisBalance = balance[pledgeStatus][tokenAddress];
+        if (!thisBalance) return;
+
+        thisBalance.pledges.forEach(pledgeId => {
+          const pledge = pledges[pledgeId];
+
+          const pledgeDonations = donations.filter(d => d.pledgeId.toNumber() === pledgeId);
+
+          let donationsAmount = new BigNumber(0);
+
+          pledgeDonations.forEach(d => {
+            if (d.status !== donationStatus)
+              console.error(
+                `Donation ${d._id} status should be ${donationStatus} but is ${d.status}`,
+              );
+            donationsAmount = donationsAmount.plus(d.amount.toString());
+          });
+
+          if (pledge.amount !== donationsAmount.toFixed()) {
+            console.warn(
+              `Pledge ${pledgeId} amount is ${pledge.amount} but sum of ${
+                pledgeDonations.length
+              } donations is ${donationsAmount.toFixed()}`,
+            );
+          }
+        });
+      });
+    });
   });
 };
 
 const main = async (updateCache, findConflict, fixConflicts = false) => {
   try {
-    const status = await getStatus(updateCache);
+    const { status } = await getBlockchainData(updateCache);
 
     if (!findConflict) return;
 
     const { pledges, admins } = status;
 
-    const adminProjects = new Set();
+    const projectAdmins = new Set();
     for (let i = 1; i < admins.length; i += 1) {
       if (admins[i].type === 'Project') {
-        adminProjects.add(i);
+        projectAdmins.add(i);
       }
     }
 
-    const projectBalanceMap = new Map();
+    const projectPledgeMap = new Map();
+    const pledgeChildrenMap = new Map();
 
     for (let i = 1; i < pledges.length; i += 1) {
       const pledge = pledges[i];
-      const { amount, owner, pledgeState } = pledge;
+      const { amount, owner, pledgeState, oldPledge } = pledge;
       const token = pledge.token.toLowerCase();
 
-      if (pledgeState !== 'Pledged' || !adminProjects.has(Number(owner))) {
+      if (!projectAdmins.has(Number(owner))) {
         // console.warn(`owner ${owner} is not a project`);
         continue;
       }
 
-      const balance = projectBalanceMap.get(owner) || {};
-      const donationCounter = balance[token] || { pledges: [], amount: new BigNumber(0) };
-      if (amount !== '0') {
-        donationCounter.pledges.push(i);
-        donationCounter.amount = donationCounter.amount.plus(amount);
+      if (oldPledge !== '0') {
+        const children = pledgeChildrenMap.get(Number(oldPledge)) || [];
+        children.push(i);
+        pledgeChildrenMap.set(Number(oldPledge), children);
       }
-      balance[token] = donationCounter;
-      projectBalanceMap.set(owner, balance);
+
+      const balance = projectPledgeMap.get(owner) || { Pledged: {}, Paying: {}, Paid: {} };
+      const donationCounter = balance[pledgeState][token] || {
+        pledges: [],
+        amount: new BigNumber(0),
+      };
+      donationCounter.pledges.push(i);
+      donationCounter.amount = donationCounter.amount.plus(amount);
+      balance[pledgeState][token] = donationCounter;
+      projectPledgeMap.set(owner, balance);
     }
 
     /*
@@ -215,8 +298,8 @@ const main = async (updateCache, findConflict, fixConflicts = false) => {
       console.log('Connected to Mongo');
 
       Promise.all([
-        findEntityConflicts(Milestones, projectBalanceMap, fixConflicts),
-        findEntityConflicts(Campaigns, projectBalanceMap, fixConflicts),
+        findEntityConflicts(Milestones, projectPledgeMap, fixConflicts, pledges, pledgeChildrenMap),
+        findEntityConflicts(Campaigns, projectPledgeMap, fixConflicts, pledges, pledgeChildrenMap),
       ]).then(() => process.exit());
     });
   } catch (e) {
@@ -225,6 +308,6 @@ const main = async (updateCache, findConflict, fixConflicts = false) => {
   }
 };
 
-main(false, true, true)
+main(false, true, false)
   .then(() => {})
   .catch(() => process.exit(1));
