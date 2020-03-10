ab45dcae75089962d92ff96a266f057ff61cdf74
Amin Latifi <a.latifi.al@gmail.com>
2020-03-10 18:54:13 +0330

    Stream used to create response. Some refactoring is done.

--- src/services/csv/campaigncsv.service.js
+++ src/services/csv/campaigncsv.service.js
@@ -1,57 +1,38 @@
-const { ObjectID } = require('mongodb');
-const json2csv = require('json2csv');
+// const { Readable, Transform } = require('stream');
+const Stream = require('stream');
+const { Transform } = require('json2csv');
 
 const fields = [
-  'Action Date',
-  'Sender Name',
-  'Sender Address',
-  'Recipient',
-  'Recipient Address',
-  'Currency',
-  'Amount',
-  'Action',
-  'Link to Milestone',
-  'Link to Recipient',
-  'Link to Donor Profile Page',
-  'Accessible Campaign Balance in ETH',
-  'Accessible Campaign Balance in DAI',
-  'Link to Rinkeby ',
-  'Link to Mainnet',
+  {
+    label: 'Giver Address',
+    value: 'from',
+    default: 'NULL',
+  },
+  {
+    label: 'Giver Name',
+    value: 'fromName',
+    default: 'NULL',
+  },
+  'to',
+  'toName',
+  'txHash',
+  'amount',
+  'action',
+  'date',
+  // 'totalCampaignAmount',
 ];
 
 class CsvItem {
-  constructor(
-    actionDate,
-    fromName,
-    from,
-    toName,
-    to,
-    currency,
-    amount,
-    action,
-    linkMilestone,
-    linkRecipient,
-    linkDonor,
-    ethCampBalance,
-    daiCampBalance,
-    linkRink,
-    linkMain,
-  ) {
-    this.actionDate = actionDate;
-    this.fromName = fromName;
+  constructor(from, fromName, to, toName, txHash, amount, action, date) {
     this.from = from;
-    this.toName = toName;
+    this.fromName = fromName;
     this.to = to;
-    this.currency = currency;
+    this.toName = toName;
+    this.txHash = txHash;
     this.amount = amount;
     this.action = action;
-    this.linkMilestone = linkMilestone;
-    this.linkRecipient = linkRecipient;
-    this.linkDonor = linkDonor;
-    this.ethCampBalance = ethCampBalance;
-    this.daiCampBalance = daiCampBalance;
-    this.linkRink = linkRink;
-    this.linkMain = linkMain;
+    this.date = date;
+    // this.totalCampaignAmount = totalCampaignAmount;
   }
 
   returnJSON() {
@@ -75,133 +56,78 @@ class CsvItem {
   }
 }
 
-async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index += 1) {
-    // eslint-disable-next-line no-await-in-loop
-    await callback(array[index], index, array);
-  }
-}
-
-async function itemsFromDonations(donations, dappUrl, usersService, campaignService) {
-  const csvItems = [];
-  let totalEthDonated = 0;
-  let totalDaiDonated = 0;
-  await asyncForEach(donations, async donation => {
-    const donationData = await usersService.find({
-      query: {
-        address: donation.giverAddress,
-      },
-    });
-    const oId = new ObjectID(donation.ownerTypeId);
-    const campaignData = await campaignService.find({
-      query: {
-        _id: oId,
-      },
-    });
-    const campaignName = campaignData.data[0].title;
-    // console.log(campaignName)
-    const { symbol } = donation.token;
-    const donationUser = donationData.data[0];
-    let donatorName = '';
-    if (donationUser.name === '' || !donationUser.name) {
-      donatorName = 'Anonymous';
-    } else {
-      donatorName = donationUser.name;
-    }
-    // let action = '';
-    // const linkMilestone = `${dappUrl}/campaigns/${donation.ownerTypeId}`;
-    const donorProfile = `https://${dappUrl}/profile/${donation.giverAddress}`;
-    const tokenAmount = donation.amount / 10 ** donation.token.decimals;
-    const mainLink = `https://etherscan.io/tx/${donation.homeTxHash}`;
-    const rinkLink = `https://rinkeby.etherscan.io/tx/${donation.txHash}`;
-    if (symbol === 'ETH') {
-      totalEthDonated += tokenAmount;
-    } else {
-      totalDaiDonated += tokenAmount;
-    }
-    const csvItem = new CsvItem(
-      donation.commitTime,
-      donatorName,
-      donation.giverAddress,
-      campaignName,
-      donation.ownerTypeId,
-      symbol,
-      donation.usdValue,
-      'Donated',
-      '-',
-      '-',
-      donorProfile,
-      totalEthDonated,
-      totalDaiDonated,
-      rinkLink,
-      mainLink,
-    );
-    // console.log(csvItem)
-    csvItems.push(csvItem.returnJSON());
-  });
-  return csvItems;
-}
-
-function toCSV(jsonCore) {
-  const json = Object.values(jsonCore);
-  let csv = '';
-  const keys = (json[0] && Object.keys(json[0])) || [];
-  csv += `${keys.join(',')}\n`;
-  // eslint-disable-next-line no-restricted-syntax
-  for (const line of json) {
-    csv += `${keys.map(key => line[key]).join(',')}\n`;
-  }
-  return csv;
-}
-
 module.exports = function registerService() {
   const app = this;
-  // const donationService = app.service('donations');
-  // const result = await donationService.find({
-  //     query: {
-  //         status: { $ne: 'Failed' },
-  //         $or: [{ intendedProjectTypeId: id }, { ownerTypeId: id }],
-  //         ownerTypeId: id,
-  //         isReturn: false,
-  //         $sort: { usdValue: -1, createdAt: -1 },
-  //         $limit:0,
-  //         $skip:0,
-  //       },
-  //       schema: 'includeTypeAndGiverDetails',
-  // });
 
   const donationService = app.service('donations');
-  const campaignService = app.service('campaigns');
-  const usersService = app.service('users');
-  const dappUrl = app.get('dappUrl');
+  // const campaignService = app.service('campaigns');
+  const milestoneService = app.service('milestones');
+  // const usersService = app.service('users');
 
+  const transformDonation = new Stream.Transform({
+    objectMode: true,
+    transform(donation, _, callback) {
+      const csvItem = new CsvItem(
+        donation.giverAddress,
+        donation.giver.name,
+        donation.ownerTypeId,
+        donation.ownerEntity.title,
+        donation.txHash,
+        donation.amount,
+        'Donated',
+        donation.createdAt,
+      );
+      callback(null, csvItem.returnJSON());
+    },
+  });
+  const getDonationStream = async id => {
+    const milestones = await milestoneService.find({
+      query: {
+        campaignId: id,
+        $select: ['id'],
+      },
+      paginate: false,
+    });
+
+    const result = await donationService.find({
+      query: {
+        status: 'Committed',
+        ownerTypeId: { $in: [id, ...milestones.map(m => m._id)] },
+        // $select: [
+        //   '_id',
+        //   'giverAddress',
+        //   'ownerType',
+        //   'ownerTypeId',
+        //   'txHash',
+        //   'amount',
+        //   'createdAt',
+        // ],
+      },
+      paginate: false,
+      schema: 'includeTypeAndGiverDetails',
+    });
+    const readable = new Stream.Readable({
+      read() {},
+      objectMode: true,
+    });
+    result.forEach(d => {
+      return readable.push(d);
+    });
+    readable.push(null);
+    return readable;
+  };
   const csvService = {
     async get(id) {
-      const result = await donationService.find({
-        query: {
-          status: 'Committed',
-          ownerTypeId: id,
-        },
-      });
-      let csvItems = [];
-      csvItems = await toCSV(
-        await itemsFromDonations(result.data, dappUrl, usersService, campaignService),
-      );
-      // console.log(csvItems)
-      return csvItems;
+      const donationStream = await getDonationStream(id);
+
+      return donationStream.pipe(transformDonation);
     },
   };
 
   // Initialize our service with any options it requires
   app.use('/campaigncsv', csvService, (req, res) => {
     const result = res.data;
-    const { data } = result; // will be either `result` as an array or `data` if it is paginated
-    const csv = json2csv.parse({
-      data,
-      fields,
-    });
-
-    res.type('csv');
-    res.end(csv);
+    const json2csv = new Transform({ fields }, { objectMode: true });
+    result.pipe(json2csv).pipe(res);
   });
 };
