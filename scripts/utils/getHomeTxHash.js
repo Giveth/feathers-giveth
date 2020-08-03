6bb3db504ba77de1cdae6956d1f9a628e9fb4952
Amin Latifi <a.latifi.al@gmail.com>
2020-08-03 20:20:33 +0430

    Used modified log.data to decode events. Just kept homeTx data which is needed. * Upgraded Web3

--- scripts/utils/getHomeTxHash.js
+++ scripts/utils/getHomeTxHash.js
@@ -41,7 +41,12 @@ async function getHomeTxHash(web3, txHash) {
 
   if (logs.length === 0) return undefined;
 
-  const log = logs[0];
+  const [log] = logs;
+  const { data } = log;
+  // Just keep homeTx
+  log.data = `0x${'0'.repeat(128)}${data.substring(130, 130 + 64)}${'0'.repeat(
+    data.length - 2 - 128 - 64,
+  )}`;
 
   const topic = topics.find(t => t.hash === log.topics[0]);
   const event = decoders[topic.name](log);
