952465954a72b606455c3e0ca7b1980c26fe80ee
Amin Latifi <a.latifi.al@gmail.com>
2020-10-06 05:00:04 +0330

    Donation aggregate service is created.

--- src/services/aggregateDonations/aggregateDonations.service.js
+++ src/services/aggregateDonations/aggregateDonations.service.js
@@ -1,37 +1,76 @@
+const { ObjectId } = require('mongoose').Types;
+const { DonationStatus } = require('../../models/donations.model');
+
 module.exports = function aggregateDonations() {
   const app = this;
   const donationsService = app.service('donations');
-  const getDonationsInfo = async delegateTypeId => {
-    const query = {
-      _aggregate: [
+  const usersService = app.service('users');
+
+  const aggregateDonationsService = {
+    async find({ query }) {
+      const { id, $limit, $skip } = query;
+      if (!id || !ObjectId.isValid(id)) {
+        return { error: 400 };
+      }
+
+      const donationModel = donationsService.Model;
+
+      const params = [
         {
           $match: {
-            delegateTypeId,
-            amountRemaining: { $ne: 0 },
-            intendedProjectId: { $exists: false },
+            status: { $in: [DonationStatus.COMMITTED, DonationStatus.WAITING] },
+            $or: [
+              { ownerTypeId: id }, // Committed ones to project
+              { intendedProjectTypeId: id }, // Delegated via DAC
+              {
+                delegateTypeId: id,
+                intendedProjectId: { $exists: false },
+              }, // Dac donations
+            ],
+            amount: { $ne: 0 },
             isReturn: false,
           },
         },
-        { $sort: { totalAmount: 1 } },
         {
           $group: {
             _id: '$giverAddress',
             totalAmount: { $sum: '$usdValue' },
             count: { $sum: 1 },
+            donations: { $push: '$_id' },
           },
         },
-      ],
-    };
-    return donationsService.find({
-      paginate: false,
-      query,
-    });
-  };
-  const aggregateDonationsService = {
-    async find({ query }) {
-      const { delegateTypeId } = query;
-      const result = await getDonationsInfo(delegateTypeId);
-      return result;
+        { $sort: { totalAmount: -1 } },
+      ];
+
+      if ($skip) params.push({ $skip: Number($skip) });
+      if ($limit) params.push({ $limit: Number($limit) });
+
+      const result = await donationModel.aggregate(params);
+
+      const promises = result.map(async item => {
+        const [donations, [giver] = []] = await Promise.all([
+          donationsService.find({
+            paginate: false,
+            query: {
+              _id: { $in: item.donations },
+              $sort: { createAt: -1 },
+            },
+          }),
+          usersService.find({
+            paginate: false,
+            query: {
+              address: item._id,
+            },
+          }),
+        ]);
+
+        item.donations = donations;
+        item.giver = giver;
+
+        return item;
+      });
+
+      return Promise.all(promises);
     },
   };
   app.use('/aggregateDonations', aggregateDonationsService);
