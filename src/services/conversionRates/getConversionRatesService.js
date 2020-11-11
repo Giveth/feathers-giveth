3334f340cb247d19491028017702023a79c4ff89
renjer <mranjbar.z2993@gmail.com>
2020-11-11 21:18:47 +0330

    Applying pull request review changes
    Add error handling for tokens that aren't in tokens whitelist

--- src/services/conversionRates/getConversionRatesService.js
+++ src/services/conversionRates/getConversionRatesService.js
@@ -1,5 +1,6 @@
 const rp = require('request-promise');
 const logger = require('winston');
+const errors = require('@feathersjs/errors');
 
 const MINUTE = 1000 * 60;
 
@@ -106,11 +107,11 @@ const _getRatesCoinGecko = async (
  * @param {Number} timestamp   Timestamp for which the value should be retrieved
  * @param {Array}  ratestToGet Rates that are missing in the DB and should be retrieved
  * @param {Array}  stableCoins coins whose value equal one usd
- * @param {String}  rateEqSymbol This field use for tokens like WBTC that has equivalent value of another currency like BTC
  *
  * @return {Object} Rates object in format { EUR: 241, USD: 123 }
  */
-const _getRatesCryptocompare = async (timestamp, ratesToGet, symbol, stableCoins, rateEqSymbol) => {
+const _getRatesCryptocompare = async (timestamp, ratesToGet, symbol, stableCoins) => {
+  logger.debug("_getRatesCryptocompare ",{timestamp, ratesToGet, symbol, stableCoins})
   logger.debug(`Fetching coversion rates from crypto compare for: ${ratesToGet}`);
   const timestampMS = Math.round(timestamp / 1000);
 
@@ -118,11 +119,11 @@ const _getRatesCryptocompare = async (timestamp, ratesToGet, symbol, stableCoins
   //TODO I think this can be removed because in else statement this field will set
   rates[symbol] = 1;
 
-  const requestSymbol = stableCoins.includes(symbol) ? 'USD' : rateEqSymbol ||symbol;
+  const requestSymbol = stableCoins.includes(symbol) ? 'USD' : symbol;
   // Fetch the conversion rate
   const promises = ratesToGet.map(async r => {
     const rateSymbol = stableCoins.includes(r) ? 'USD' : r;
-    if (rateSymbol !== requestSymbol && rateSymbol !== symbol) {
+    if (rateSymbol !== requestSymbol) {
       const resp = JSON.parse(
         await rp(
           `https://min-api.cryptocompare.com/data/dayAvg?fsym=${requestSymbol}&tsym=${rateSymbol}&toTs=${timestampMS}&extraParams=giveth`,
@@ -267,7 +268,7 @@ const _saveToDB = (app, timestamp, rates, symbol, _id = undefined) => {
  *
  * @return {Promise} Promise that resolves to object {timestamp, rates: { EUR: 100, USD: 90 } }
  */
-const getConversionRates = async (app, requestedDate, requestedSymbol = 'ETH') => {
+const getConversionRates = async (app, requestedDate, symbol = 'ETH') => {
   // Get yesterday date from today respecting UTC
   const yesterday = new Date(new Date().setUTCDate(new Date().getUTCDate() - 1));
   const yesterdayUTC = yesterday.setUTCHours(0, 0, 0, 0);
@@ -284,17 +285,13 @@ const getConversionRates = async (app, requestedDate, requestedSymbol = 'ETH') =
   const stableCoins = app.get('stableCoins') || [];
 
   const tokens =app.get('tokenWhitelist');
-  let coingeckoId = '';
-  let rateEqSymbol = undefined;
-  tokens.forEach(token => {
-    if (token.symbol === requestedSymbol) {
-      //This field needed for PAN currency
-      coingeckoId = token.coingeckoid;
-
-      rateEqSymbol = token.rateEqSymbol
-    }
-  });
-
+  const token = tokens.find(token => token.symbol === symbol);
+  if (!token) {
+    throw new errors.BadRequest(`Token with symbol ${symbol} is not in whitelist`)
+  }
+  //This field needed for PAN currency
+  const coingeckoId = token.coingeckoid;
+  const requestedSymbol = token.rateEqSymbol || symbol;
   logger.debug(`request eth conversion for timestamp ${timestamp}`);
 
   // Check if we already have this exchange rate for this timestamp, if not we save it
@@ -321,8 +318,7 @@ const getConversionRates = async (app, requestedDate, requestedSymbol = 'ETH') =
         timestamp,
         unknownRates,
         requestedSymbol,
-        stableCoins,
-        rateEqSymbol
+        stableCoins
       );
     }
 
