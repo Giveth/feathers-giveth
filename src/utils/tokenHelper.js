c8ce928e5bd7fbee4f6fdf74c1dc98d84ab3fa58
Amin Latifi <a.latifi.al@gmail.com>
2020-12-27 19:04:58 +0330

    Replaced incorrect variable.

--- src/utils/tokenHelper.js
+++ src/utils/tokenHelper.js
@@ -23,7 +23,7 @@ function getTokenBySymbol(symbol) {
     getWhiteListTokens().forEach(token => {
       tokensBySymbols[token.symbol] = token;
     });
-    tokensByAddress[ANY_TOKEN.symbol] = ANY_TOKEN;
+    tokensBySymbols[ANY_TOKEN.symbol] = ANY_TOKEN;
   }
   return tokensBySymbols[symbol] || { symbol };
 }
