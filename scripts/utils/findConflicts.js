233de079b42cdc613b965523410450a9fedf3e72
Amin Latifi <a.latifi.al@gmail.com>
2021-01-07 00:50:21 +0330

    Fixed bug on setting status of donations resulting from approve/reject on simulations script * Fixed error on aggregateDonations.service.js

--- scripts/utils/findConflicts.js
+++ scripts/utils/findConflicts.js
@@ -436,7 +436,7 @@ const fetchDonationsInfo = async () => {
           ownerId,
           ownerType,
           ownerTypeId,
-          intendedProjectId,
+          intendedProjectId: String(intendedProjectId),
           giverAddress,
           pledgeId: pledgeId.toString(),
           tokenAddress,
@@ -575,7 +575,7 @@ const handleFromDonations = async (from, to, amount, transactionHash) => {
 
         if (fixConflicts) {
           logger.debug('Updating...');
-          await Donations.update(
+          await Donations.updateOne(
             { _id: toFixDonation._id },
             { status: toFixDonation.status, pledgeId: to },
           ).exec();
@@ -628,10 +628,19 @@ const handleFromDonations = async (from, to, amount, transactionHash) => {
 
           const min = BigNumber.min(item.amountRemaining, fromAmount);
           item.amountRemaining = item.amountRemaining.minus(min);
+          fromAmount = fromAmount.minus(min);
+
           if (item.amountRemaining.isZero()) {
             consumedCandidates += 1;
+
+            // It's approve or reject
+            if (item.status === DonationStatus.TO_APPROVE) {
+              item.status =
+                toPledge.owner === item.intendedProjectId
+                  ? DonationStatus.COMMITTED
+                  : DonationStatus.REJECTED;
+            }
           }
-          fromAmount = fromAmount.minus(min);
           logger.debug(
             `Amount ${min.toFixed()} is reduced from ${JSON.stringify(
               { ...item, amountRemaining: item.amountRemaining.toFixed() },
@@ -907,7 +916,7 @@ const handleToDonations = async ({
     if (toDonation.mined === false) {
       logger.error(`Donation ${toDonation._id} mined flag should be true`);
       logger.debug('Updating...');
-      await Donations.update({ _id: toDonation._id }, { mined: true }).exec();
+      await Donations.updateOne({ _id: toDonation._id }, { mined: true }).exec();
       toDonation.mined = true;
     }
 
@@ -922,7 +931,7 @@ const handleToDonations = async ({
       if (fixConflicts) {
         logger.debug('Updating...');
         toDonation.parentDonations = usedFromDonations;
-        await Donations.update(
+        await Donations.updateOne(
           { _id: toDonation._id },
           { parentDonations: usedFromDonations },
         ).exec();
@@ -932,7 +941,7 @@ const handleToDonations = async ({
     if (toDonation.isReturn !== isReturn) {
       logger.error(`Donation ${toDonation._id} isReturn flag should be ${isReturn}`);
       logger.debug('Updating...');
-      await Donations.update({ _id: toDonation._id }, { isReturn }).exec();
+      await Donations.updateOne({ _id: toDonation._id }, { isReturn }).exec();
       toDonation.isReturn = isReturn;
     }
 
@@ -943,7 +952,7 @@ const handleToDonations = async ({
         `Donation ${toDonation._id} usdValue is ${usdValue} but should be updated to ${toDonation.usdValue}`,
       );
       logger.debug('Updating...');
-      await Donations.update({ _id: toDonation._id }, { usdValue: toDonation.usdValue }).exec();
+      await Donations.updateOne({ _id: toDonation._id }, { usdValue: toDonation.usdValue }).exec();
     }
 
     toDonation.txHash = transactionHash;
@@ -1062,7 +1071,7 @@ const fixConflictInDonations = unusedDonationMap => {
             logger.debug('Updating...');
             const { cutoff } = symbolDecimalsMap[getTokenByAddress(tokenAddress).symbol];
             promises.push(
-              Donations.update(
+              Donations.updateOne(
                 { _id },
                 {
                   $set: {
@@ -1092,7 +1101,7 @@ const fixConflictInDonations = unusedDonationMap => {
           );
           if (fixConflicts) {
             logger.debug('Updating...');
-            promises.push(Donations.update({ _id }, { status }).exec());
+            promises.push(Donations.updateOne({ _id }, { status }).exec());
           }
         }
       }
