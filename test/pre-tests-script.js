1d84eab1fe38e2aa05e8c951f279d8517fdca907
Renjer <mranjbar.z2993@gmail.com>
2021-01-19 08:17:59 +0330

    Fix get conversionRates for PAN coin
    Test case written
    related to Giveth/giveth-dapp#1555

--- test/pre-tests-script.js
+++ test/pre-tests-script.js
@@ -7,16 +7,21 @@ function sleep(ms) {
 before(async () => {
   try {
     // await mongoServer.getUri();
+    console.log('pre-test 0')
+
     await seedData();
+    console.log('test db restored')
 
     // If we require startServer before initializing mongo the server will not responding, I dont know the reason yet
     /* eslint-disable-next-line */
     const { startServer } = require('../src/server');
+    console.log('starting server')
     // we need to wait after setting up of mongoServer we starServer
     await startServer();
-
+    console.log('feathers server is up')
     // This is because it takes seconds to feather server starts
-    await sleep(3000);
+    await sleep(1000);
+    console.log('going to run tests .....')
     // console.log('after running feather js', mongoUri);
   } catch (e) {
     console.log('error in beforeAll', e);
