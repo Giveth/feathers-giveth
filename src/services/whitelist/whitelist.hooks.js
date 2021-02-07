e2d7429ee7ba120fa9d95d021606e377b924c304
Renjer <mranjbar.z2993@gmail.com>
2021-02-07 23:03:21 +0330

    Check whitelist with roleUtils functions instead of config

--- src/services/whitelist/whitelist.hooks.js
+++ src/services/whitelist/whitelist.hooks.js
@@ -1,32 +1,12 @@
 const { getTokenBySymbol } = require('../../utils/tokenHelper');
 
-const getUsersByAddress = (app, addresses) =>
-  app
-    .service('/users')
-    .find({
-      paginate: false,
-      query: {
-        address: { $in: addresses },
-        $select: ['name', 'email', 'address', 'avatar'],
-      },
-    })
-    .then(users =>
-      users.concat(
-        addresses
-          .filter(a => !users.find(u => u.address === a))
-          .map(address => ({
-            address,
-          })),
-      ),
-    );
-
 const getWhitelist = () => context => {
   const { app } = context;
 
   // fetch whitelisted addresses from default.json
-  const reviewers = app.get('useReviewerWhitelist') ? app.get('reviewerWhitelist') : [];
-  const delegates = app.get('useDelegateWhitelist') ? app.get('delegateWhitelist') : [];
-  const projectOwners = app.get('useProjectOwnerWhitelist') ? app.get('projectOwnerWhitelist') : [];
+  // const reviewers = app.get('useReviewerWhitelist') ? app.get('reviewerWhitelist') : [];
+  // const delegates = app.get('useDelegateWhitelist') ? app.get('delegateWhitelist') : [];
+  // const projectOwners = app.get('useProjectOwnerWhitelist') ? app.get('projectOwnerWhitelist') : [];
   const tokenWhitelist = app.get('tokenWhitelist');
   let activeTokenWhitelist =
     app.get('activeTokenWhitelist') &&
@@ -38,25 +18,13 @@ const getWhitelist = () => context => {
   }
   const fiatWhitelist = app.get('fiatWhitelist');
   const nativeCurrencyWhitelist = app.get('nativeCurrencyWhitelist');
-
-  // find all the users
-  return Promise.all([
-    getUsersByAddress(app, reviewers),
-    getUsersByAddress(app, delegates),
-    getUsersByAddress(app, projectOwners),
-  ]).then(([reviewerUsers, delegateUsers, projectOwnerUsers]) => {
-    context.result = {
-      reviewerWhitelist: reviewerUsers,
-      delegateWhitelist: delegateUsers,
-      projectOwnerWhitelist: projectOwnerUsers,
-      tokenWhitelist,
-      activeTokenWhitelist,
-      fiatWhitelist,
-      nativeCurrencyWhitelist,
-    };
-
-    return context;
-  });
+  context.result = {
+    tokenWhitelist,
+    activeTokenWhitelist,
+    fiatWhitelist,
+    nativeCurrencyWhitelist,
+  };
+  return context;
 };
 
 module.exports = {
