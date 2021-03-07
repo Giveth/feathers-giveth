8b4e24ce874aacf0d435096febcb406baeea57d1
Amin Latifi <a.latifi.al@gmail.com>
2021-03-07 13:39:29 +0330

    Update 20210204190556-add-slug-to-projects.js

--- migrations/20210204190556-add-slug-to-projects.js
+++ migrations/20210204190556-add-slug-to-projects.js
@@ -1,10 +1,20 @@
 const async = require('async');
 
 function slugify(title) {
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------';
+  const p = new RegExp(a.split('').join('|'), 'g');
+
   return title
-    .replace(/[^a-zA-Z\d\s_()-./\\]/g, '')
-    .replace(/(\s|_|\(|\)|\/|\\|\.)+/g, '-')
-    .toLowerCase();
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w-]+/g, '') // Remove all non-word characters
+    .replace(/--+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, ''); // Trim - from end of text
 }
 
 async function createSlug(id, title, collection) {
