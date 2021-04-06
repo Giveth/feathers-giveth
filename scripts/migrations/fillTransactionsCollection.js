5adf4303f215a130ebb166e5dbc918141b52e195
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-04-06 08:08:04 +0430

    Add script for filling transaction collection

---
+++ scripts/migrations/fillTransactionsCollection.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
+
+const config = require(`config`);
+
+require('mongoose-long')(mongoose);
+require('../../src/models/mongoose-bn')(mongoose);
+const { getTransaction, getWeb3, getHomeWeb3 } = require('../../src/blockchain/lib/web3Helpers');
+
+const appFactory = () => {
+  const data = {};
+  return {
+    get(key) {
+      return data[key];
+    },
+    set(key, val) {
+      data[key] = val;
+    },
+  };
+};
+
+const app = appFactory();
+app.set('mongooseClient', mongoose);
+app.getWeb3 = getWeb3.bind(null, app);
+app.getHomeWeb3 = getHomeWeb3.bind(null, app);
+app.set('blockchain', config.get('blockchain'));
+const Events = require('../../src/models/events.model').createModel(app);
+const Donations = require('../../src/models/donations.model').createModel(app);
+const Transactions = require('../../src/models/transactions.model').createModel(app);
+
+app.set('transactionsModel', Transactions);
+
+const mongoUrl = config.mongodb;
+console.log('url:', mongoUrl);
+mongoose.connect(mongoUrl);
+const db = mongoose.connection;
+
+const handleEvents = async () => {
+  await Events.find({})
+    .cursor()
+    .eachAsync(
+      async ({ transactionHash }) => {
+        await getTransaction(app, transactionHash);
+        console.log('Getting event transaction');
+      },
+      {
+        parallel: 50,
+      },
+    );
+  await Donations.find({})
+    .cursor()
+    .eachAsync(
+      async ({ txHash, homeTxHash }) => {
+        if (txHash) {
+          await getTransaction(app, txHash);
+        }
+        if (homeTxHash) {
+          await getTransaction(app, homeTxHash, true);
+        }
+        console.log('Getting donation transaction');
+      },
+      {
+        parallel: 50,
+      },
+    );
+};
+
+db.on('error', err => console.error('Could not connect to Mongo', err));
+
+// once mongo connected, start migration
+db.once('open', () => {
+  console.log('Connected to Mongo');
+  handleEvents();
+});
