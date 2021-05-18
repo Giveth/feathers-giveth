b2e2b85c8339666014399ce22b69ed5002438c3a
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-05-18 15:27:59 +0430

    Remove bridgePayment stuff from existing donations, to be fill again

--- migrations/20210518101611-change_all_paymentExecuted_events_to_waiting_and_revert_processing_side_effects.js
+++ migrations/20210518101611-change_all_paymentExecuted_events_to_waiting_and_revert_processing_side_effects.js
@@ -3,15 +3,25 @@ module.exports = {
     // The purpose of this migration is revert all changes that are related to PaymentExexuted and PaymentAuthorized events
     // then process all these events again (by changing their status to Waiting)
     await db.collection('conversations').deleteMany({ messageContext: 'payout' });
-    await db.collection('users').updateMany({}, { $unset: { gasPaidUsdValue: true } });
-    await db.collection('milestones').updateMany({}, { $unset: { gasPaidUsdValue: true } });
-    await db.collection('campaigns').updateMany({}, { $unset: { gasPaidUsdValue: true } });
+    await db.collection('users').updateMany({}, { $unset: { gasPaidUsdValue: 1 } });
+    await db.collection('milestones').updateMany({}, { $unset: { gasPaidUsdValue: 1 } });
+    await db.collection('campaigns').updateMany({}, { $unset: { gasPaidUsdValue: 1 } });
     await db
       .collection('events')
       .updateMany(
         { event: { $in: ['PaymentExecuted', 'PaymentAuthorized'] }, status: 'Processed' },
         { $set: { status: 'Waiting' } },
       );
+    await db.collection('donations').updateMany(
+      {},
+      {
+        $unset: {
+          bridgePaymentExecutedTime: 1,
+          bridgePaymentExecutedTxHash: 1,
+          bridgePaymentAuthorizedTxHash: 1,
+        },
+      },
+    );
   },
 
   async down(_db, _client) {
