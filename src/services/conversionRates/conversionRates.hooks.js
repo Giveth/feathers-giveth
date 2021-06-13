cff494ee190eb94c957b82515d2b3eefe3e65935
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-06-13 16:26:41 +0430

    Stop supporting conversion rate by transaction hash

--- src/services/conversionRates/conversionRates.hooks.js
+++ src/services/conversionRates/conversionRates.hooks.js
@@ -1,5 +1,4 @@
 const { disallow } = require('feathers-hooks-common');
-const errors = require('@feathersjs/errors');
 
 const onlyInternal = require('../../hooks/onlyInternal');
 const {
@@ -7,7 +6,6 @@ const {
   getHourlyCryptoConversion,
   getHourlyMultipleCryptoConversion,
 } = require('./getConversionRatesService');
-const { getTransaction } = require('../../blockchain/lib/web3Helpers');
 
 const findConversionRates = () => async context => {
   const { app, params } = context;
@@ -15,34 +13,10 @@ const findConversionRates = () => async context => {
   // return context to avoid recursion
   // getConversionRates also calls this hook
   if (params.internal) return context;
-  const {
-    date: queryDate,
-    to,
-    symbol,
-    from,
-    interval: queryInterval,
-    txHash,
-    isHome,
-  } = params.query;
+  const { date: queryDate, to, symbol, from, interval: queryInterval } = params.query;
 
-  let date = Number(queryDate);
-  let interval = queryInterval;
-
-  if (txHash) {
-    let error;
-    try {
-      const tx = await getTransaction(app, txHash, isHome === 'true');
-      if (tx) {
-        date = tx.timestamp;
-        interval = 'hourly';
-      }
-    } catch (e) {
-      error = e;
-    }
-    if (error) throw new errors.BadRequest(`Invalid tx ${error}`);
-  }
-
-  if (interval === 'hourly') {
+  const date = Number(queryDate);
+  if (queryInterval === 'hourly') {
     if (Array.isArray(to)) {
       return getHourlyMultipleCryptoConversion(app, date, from, to).then(res => {
         context.result = res;
