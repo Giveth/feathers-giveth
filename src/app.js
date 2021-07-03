38554c9388abc99a8cc0ac3c7c6294ec5d8d7365
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-07-03 10:38:06 +0430

    Add environment and release to sentry when initializing

--- src/app.js
+++ src/app.js
@@ -24,9 +24,10 @@ const { configureAuditLog } = require('./auditLog/feathersElasticSearch');
 const channels = require('./channels');
 
 const app = express(feathers());
-
 Sentry.init({
   dsn: config.sentryDsn,
+  environment: process.env.NODE_ENV,
+  release: `Giveth-Feathers@${process.env.npm_package_version}`,
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
