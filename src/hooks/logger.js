82e586f019a1318f737060136f16d8099fa5fb9c
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-07-03 10:17:02 +0430

    Config Sentry to just capture non-featehrs error

--- src/hooks/logger.js
+++ src/hooks/logger.js
@@ -26,7 +26,10 @@ module.exports = function responseLoggerHook() {
 
     if (hook.error) {
       const e = hook.error;
-      Sentry.captureException(e);
+      // for making sure the feathers errors like unAuthorized wouldn't capture as exceptions
+      if (e.type !== 'FeathersError'){
+        Sentry.captureException(e);
+      }
       delete e.hook;
 
       if (hook.path === 'authentication') {
