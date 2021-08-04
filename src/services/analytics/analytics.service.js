d8a2c89bdbb95516eea7e0540707aa57dff67b3e
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-08-04 20:43:44 +0430

    Add swagger for /analytics
    related to Giveth/giveth-dapp#2286

--- src/services/analytics/analytics.service.js
+++ src/services/analytics/analytics.service.js
@@ -9,7 +9,39 @@ module.exports = function analytics() {
       return result;
     },
   };
-  // TODO write swagger
+
+  analyticsService.docs = {
+    operations: {
+      update: false,
+      patch: false,
+      remove: false,
+      find: false,
+      create: {
+        description: 'For subscribe and unsubscribe call this endpoint with enabled',
+      },
+    },
+    definition: {
+      type: 'object',
+      properties: {
+        properties: {
+          type: 'object',
+        },
+        userId: {
+          type: 'string',
+        },
+        event: {
+          type: 'string',
+        },
+        anonymousId: {
+          type: 'string',
+        },
+        reportType: {
+          type: 'string',
+          enum: ['track', 'page'],
+        },
+      },
+    },
+  };
   app.use('/analytics', analyticsService);
   app.service('analytics').hooks(hooks);
 };
