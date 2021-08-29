0def0f7d470575ef77a8b2478650fe240c42f017
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-08-29 17:18:50 +0430

    Refactor generate swagger from mongoose model
    related to Giveth/giveth-dapp#2286

--- src/utils/swaggerUtils.test.js
+++ src/utils/swaggerUtils.test.js
@@ -16,8 +16,8 @@ const generateSwaggerDocForCRUDServiceTestCases = () => {
     assert.equal(generatedSchema.operations.remove, false);
     assert.exists(generatedSchema.definition.properties.name);
     assert.exists(generatedSchema.definition.properties.lastName);
-    assert.equal(generatedSchema.operations.find['parameters[0]'].name, '$limit');
-    assert.equal(generatedSchema.operations.find['parameters[1]'].name, '$skip');
+    assert.equal(generatedSchema.operations.find.parameters[0].name, '$limit');
+    assert.equal(generatedSchema.operations.find.parameters[1].name, '$skip');
   });
   it('should disabledMethods set method false', () => {
     const testModel = mongoose.model(`test-${generateRandomNumber(1, 10000)}`, {
@@ -36,8 +36,8 @@ const generateSwaggerDocForCRUDServiceTestCases = () => {
       age: Number,
     });
     const generatedSchema = generateSwaggerDocForCRUDService({ Model: testModel });
-    assert.equal(generatedSchema.operations.find['parameters[2]'].name, 'name');
-    assert.equal(generatedSchema.operations.find['parameters[3]'].name, 'lastName');
+    assert.equal(generatedSchema.operations.find.parameters[2].name, 'name');
+    assert.equal(generatedSchema.operations.find.parameters[3].name, 'lastName');
   });
   it('should not add non-string fields to find operation parameters', () => {
     const testModel = mongoose.model(`test-${generateRandomNumber(1, 10000)}`, {
@@ -47,10 +47,10 @@ const generateSwaggerDocForCRUDServiceTestCases = () => {
       alive: Boolean,
     });
     const generatedSchema = generateSwaggerDocForCRUDService({ Model: testModel });
-    assert.equal(generatedSchema.operations.find['parameters[2]'].name, 'name');
-    assert.equal(generatedSchema.operations.find['parameters[3]'].name, 'lastName');
-    assert.notExists(generatedSchema.operations.find['parameters[4]']);
-    assert.notExists(generatedSchema.operations.find['parameters[5]']);
+    assert.equal(generatedSchema.operations.find.parameters[2].name, 'name');
+    assert.equal(generatedSchema.operations.find.parameters[3].name, 'lastName');
+    assert.notExists(generatedSchema.operations.find.parameters[4]);
+    assert.notExists(generatedSchema.operations.find.parameters[5]);
   });
   it('should not add verified field in find operation', () => {
     const testModel = mongoose.model(`test-${generateRandomNumber(1, 10000)}`, {
@@ -60,10 +60,10 @@ const generateSwaggerDocForCRUDServiceTestCases = () => {
       alive: Boolean,
     });
     const generatedSchema = generateSwaggerDocForCRUDService({ Model: testModel });
-    assert.equal(generatedSchema.operations.find['parameters[2]'].name, 'name');
-    assert.equal(generatedSchema.operations.find['parameters[3]'].name, 'lastName');
-    assert.notExists(generatedSchema.operations.find['parameters[4]']);
-    assert.notExists(generatedSchema.operations.find['parameters[5]']);
+    assert.equal(generatedSchema.operations.find.parameters[2].name, 'name');
+    assert.equal(generatedSchema.operations.find.parameters[3].name, 'lastName');
+    assert.notExists(generatedSchema.operations.find.parameters[4]);
+    assert.notExists(generatedSchema.operations.find.parameters[5]);
   });
   it('should add verified field in find operation', () => {
     const testModel = mongoose.model(`test-${generateRandomNumber(1, 10000)}`, {
@@ -72,10 +72,10 @@ const generateSwaggerDocForCRUDServiceTestCases = () => {
       verified: Boolean,
     });
     const generatedSchema = generateSwaggerDocForCRUDService({ Model: testModel });
-    assert.equal(generatedSchema.operations.find['parameters[2]'].name, 'name');
-    assert.equal(generatedSchema.operations.find['parameters[3]'].name, 'lastName');
-    assert.equal(generatedSchema.operations.find['parameters[4]'].name, 'verified');
-    assert.exists(generatedSchema.operations.find['parameters[4]'].schema.enum);
+    assert.equal(generatedSchema.operations.find.parameters[2].name, 'name');
+    assert.equal(generatedSchema.operations.find.parameters[3].name, 'lastName');
+    assert.equal(generatedSchema.operations.find.parameters[4].name, 'verified');
+    assert.exists(generatedSchema.operations.find.parameters[4].schema.enum);
   });
 };
 
