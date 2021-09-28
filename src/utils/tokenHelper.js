47d8ac56d5e0860036f3bc2c5b9da2d9a1d36fe8
Amin Latifi <a.latifi.al@gmail.com>
2021-09-28 13:03:12 +0330

    Initialized all token helpers once

--- src/utils/tokenHelper.js
+++ src/utils/tokenHelper.js
@@ -1,9 +1,9 @@
 const config = require('config');
 const { ANY_TOKEN } = require('../blockchain/lib/web3Helpers');
 
-let tokensBySymbols;
-let tokensByAddress;
-let tokensByForeignAddress;
+const tokensBySymbols = {};
+const tokensByAddress = {};
+const tokensByForeignAddress = {};
 const validSymbols = [];
 
 const getWhiteListTokens = () => {
@@ -11,50 +11,39 @@ const getWhiteListTokens = () => {
 };
 
 function getTokenByAddress(address) {
-  if (!tokensByAddress) {
-    tokensByAddress = {};
-    getWhiteListTokens().forEach(token => {
-      tokensByAddress[token.address] = token;
-    });
-    tokensByAddress[ANY_TOKEN.address] = ANY_TOKEN;
-  }
   return tokensByAddress[address];
 }
 
 function getTokenByForeignAddress(foreignAddress) {
-  if (!tokensByForeignAddress) {
-    tokensByForeignAddress = {};
-    getWhiteListTokens().forEach(token => {
-      tokensByForeignAddress[token.foreignAddress] = token;
-    });
-    tokensByForeignAddress[ANY_TOKEN.foreignAddress] = ANY_TOKEN;
-  }
   return tokensByForeignAddress[foreignAddress];
 }
 
 function getTokenBySymbol(symbol) {
-  if (!tokensBySymbols) {
-    tokensBySymbols = {};
-    getWhiteListTokens().forEach(token => {
-      tokensBySymbols[token.symbol] = token;
-    });
-    tokensBySymbols[ANY_TOKEN.symbol] = ANY_TOKEN;
-  }
   return tokensBySymbols[symbol] || { symbol };
 }
 
-const initializeValidSymbols = () => {
-  const _set = new Set();
+const initialize = () => {
+  const _tokenSymbolSet = new Set();
+
   getWhiteListTokens().forEach(token => {
-    _set.add(token.symbol);
+    tokensByForeignAddress[token.foreignAddress] = token;
+    tokensByAddress[token.address] = token;
+    tokensBySymbols[token.symbol] = token;
+
+    _tokenSymbolSet.add(token.symbol);
     if (token.rateEqSymbol) {
-      _set.add(token.rateEqSymbol);
+      _tokenSymbolSet.add(token.rateEqSymbol);
     }
   });
-  validSymbols.push(...Array.from(_set));
+
+  tokensByForeignAddress[ANY_TOKEN.foreignAddress] = ANY_TOKEN;
+  tokensByAddress[ANY_TOKEN.address] = ANY_TOKEN;
+  tokensBySymbols[ANY_TOKEN.symbol] = ANY_TOKEN;
+
+  validSymbols.push(...Array.from(_tokenSymbolSet));
 };
 
-initializeValidSymbols();
+initialize();
 
 const getValidSymbols = () => {
   return validSymbols;
