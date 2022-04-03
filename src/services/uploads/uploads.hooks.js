1c123e6f862a822abc06a0c3a0fa46240701ac15
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-04-03 20:55:56 +0430

    Bring back the /uploads webservice

--- src/services/uploads/uploads.hooks.js
+++ src/services/uploads/uploads.hooks.js
@@ -1,101 +1,17 @@
-const dauria = require('dauria');
-const errors = require('@feathersjs/errors');
 const { disallow } = require('feathers-hooks-common');
-const { gql, GraphQLClient } = require('graphql-request');
-const config = require('config');
-const logger = require('winston');
-
-const client = new GraphQLClient(config.givethIoUrl);
-
-const TraceImageOwnerType = {
-  USER: 'USER',
-  TRACE: 'TRACE',
-  CAMPAIGN: 'CAMPAIGN',
-  DAC: 'DAC',
-};
-
-const transformFile = () => context => {
-  // delete id to prevent users specifying the file path to upload the file to
-  delete context.data.id;
-  if (!context.data.uri && context.params.file) {
-    const { file } = context.params;
-    const uri = dauria.getBase64DataURI(file.buffer, file.mimetype);
-    context.data = { uri };
-  }
-};
-
-const restrictFileType = () => context => {
-  if (!context.data.uri) throw new errors.BadRequest('Invalid request');
-
-  // note: the mimetype can be faked, however it will be saved as the faked
-  // mimetype and from manual testing, however it will be saved with an image
-  // file extension thus the browser will not execute js if
-  // the mimetype is faked as image/jpeg.
-  const parsedData = dauria.parseDataURI(context.data.uri);
-  if (!parsedData.MIME.startsWith('image/')) {
-    throw new errors.Forbidden('Only image uploads are supported');
-  }
-};
-
-const uploadToImpactGraph = () => async context => {
-  const fileData = context.data.uri;
-  const TRACE_IMAGE_UPLOAD = gql`
-      mutation traceImageUpload($fileData:String!, $user:String!, $entityId:String!, $password: String!) {
-        traceImageUpload(
-          traceFileUpload: {
-            fileDataBase64: $fileData
-            user: $user
-            entityId: $entityId
-            password: $password
-            imageOwnerType: ${TraceImageOwnerType.USER}
-          }
-        )
-      }
-  `;
-  try {
-    const result = await client.request(TRACE_IMAGE_UPLOAD, {
-      fileData,
-      user: context.params.user.address,
-      entityId: '1',
-      password: config.givethIoFileUploaderPassword,
-    });
-    context.result = result.traceImageUpload;
-  } catch (e) {
-    logger.error(e);
-  }
-};
-
-const transformCreateResponse = () => context => {
-  let { uploadsBaseUrl } = context.app.settings;
-
-  if (!uploadsBaseUrl.endsWith('/')) {
-    uploadsBaseUrl = `${uploadsBaseUrl}/`;
-  }
-
-  const { id } = context.result;
-
-  delete context.result.id;
-  delete context.result.uri;
-
-  context.result.url = `${uploadsBaseUrl}${id}`;
-
-  return context;
-};
 
 module.exports = {
   before: {
     all: [],
     get: [disallow()],
-    patch: [disallow()],
-    update: [disallow()],
-    create: [transformFile(), restrictFileType(), uploadToImpactGraph()],
+    create: [disallow()],
     remove: [disallow()],
   },
 
   after: {
     all: [],
     get: [],
-    create: [transformCreateResponse()],
+    create: [],
     remove: [],
   },
 
---
+++ src/services/uploadsByImpactGraph/uploadByImpactGraph.service.js
@@ -0,0 +1,66 @@
+/* eslint-disable class-methods-use-this */
+// Initializes the `uploads` service on path `/uploads`
+const express = require('@feathersjs/express');
+const multer = require('multer');
+const { errors } = require('@feathersjs/errors');
+const multipartTransfer = require('../../middleware/upload');
+
+const hooks = require('./uploadByImpactGraph.hooks');
+
+const notImplemented = method => {
+  throw new errors.NotImplemented(`${method} is not implemented on this service`);
+};
+class UploadByImpactGraphService {
+  find(_params) {
+    notImplemented('find');
+  }
+
+  get(_id, _params) {
+    notImplemented('get');
+  }
+
+  create(_data, _params) {}
+
+  update(_id, _data, _params) {
+    notImplemented('update');
+  }
+
+  patch(_id, _data, _params) {
+    notImplemented('patch');
+  }
+
+  remove(_id, _params) {
+    notImplemented('remove');
+  }
+
+  setup(_app, _path) {}
+}
+module.exports = function uploadByImpactGraphService() {
+  const app = this;
+
+  const multipartMiddleware = multer();
+
+  // Override the default blobService get method to directly serve the file
+  // In production, nginx will serve the file. This is a fallback if that isn't setup
+  app.use('/uploadByImpactGraph', express.static(app.get('uploads')));
+
+  const service = new UploadByImpactGraphService();
+  service.docs = {
+    operations: {
+      update: false,
+      patch: false,
+      remove: false,
+      get: false,
+      create: {
+        description: 'Currently I dont know what parameter is needed for this endpoint',
+      },
+    },
+    definition: {},
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/uploadByImpactGraph', multipartMiddleware.single('uri'), multipartTransfer, service);
+
+  // Get our initialized service so that we can register hooks and filters
+  app.service('uploadByImpactGraph').hooks(hooks);
+};
